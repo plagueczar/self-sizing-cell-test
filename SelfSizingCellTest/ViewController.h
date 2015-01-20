f8b1d332c994cf6663a5633ddbdc6f3d90c50274
Benjamin Frost <benjaminfrost@Benjamins-MacBook-Pro.local>
2015-01-20 17:08:29 +1030

    Initial Commit

---
+++ SelfSizingCellTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelfSizingCellTest
+//
+//  Created by Benjamin Frost on 20/01/2015.
+//  Copyright (c) 2015 Thirty4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SelfSizingCellTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SelfSizingCellTest
+//
+//  Created by Benjamin Frost on 20/01/2015.
+//  Copyright (c) 2015 Thirty4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
